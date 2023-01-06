248e035f37682feb2b6a8225f21d9dc7d23a4851
Garrett Smith <gasmith@uni-potsdam.de>
2023-01-06 10:11:02 +0100

    Fix Uniformization.jl

--- src/Uniformization.jl
+++ src/Uniformization.jl
@@ -3,7 +3,6 @@ module Uniformization
 using LinearAlgebra: I, eigen, diag, inv, nullspace
 using Distributions: Poisson, pdf, cdf, quantile
 
-include("utils.jl")
 include("algorithms.jl")
 export FullRateMatrix
 export uniformize, standard_uniformization, discrete_observation_times, erlangization
