f217790566bcddb3846f8c6fdecc765cbe8268a3
Garrett Smith <gasmith@uni-potsdam.de>
2023-01-06 10:28:13 +0100

    Update Uniformization.jl to export new TransitionRateMatrix.

--- src/Uniformization.jl
+++ src/Uniformization.jl
@@ -4,7 +4,7 @@ using LinearAlgebra: I, eigen, diag, inv, nullspace
 using Distributions: Poisson, pdf, cdf, quantile
 
 include("algorithms.jl")
-export FullRateMatrix
+export TransitionRateMatrix
 export uniformize, standard_uniformization, discrete_observation_times, erlangization
 
 end
